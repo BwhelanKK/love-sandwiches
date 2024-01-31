6f7b4de52a468576c654174a65468e594508d5e5
Brendan Whelan <whelan_brendan@hotmail.com>
2024-01-31 23:37:09 +0000

    Create get sales data function to get data string from user

--- run.py
+++ run.py
@@ -6,14 +6,15 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+def get_sales_data():
+    """
+    Get sales figures from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, seperated by commas.")
+    print("Example: 10, 20, 30, 40, 50, 60\n")
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+    data_str = input("Enter your data here:")
+    print(f"The data provided is: {data_str}")
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
+get_sales_data()
